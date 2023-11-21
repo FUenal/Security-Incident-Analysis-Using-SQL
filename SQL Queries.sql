0aebce75b4222d25fd7af6fce24c3e906cafaa7b
Fatih Uenal <mars.fatih@gmail.com>
2023-11-21 20:34:56 +0100

    Update SQL Queries.sql

--- SQL Queries.sql
+++ SQL Queries.sql
@@ -1,6 +1,6 @@
-# This query retrieves all columns from the log_in_attempts table where the login time is after 18:00 and the login attempt 
-# is unsuccessful (denoted by a value of 0 in the success column). It provides a way to filter and focus on failed login
-# attempts that occurred after the specified time, allowing for further investigation into potential security incidents or anomalies.
+# This query fetches all columns from the log_in_attempts table where the login time is past 18:00 and the login attempt
+# is unsuccessful (indicated by a value of 0 in the success column). It offers a means to filter and concentrate on failed login
+# attempts that took place after the specified time, facilitating a more in-depth examination of potential security incidents or anomalies.
 
 
 SELECT *
@@ -12,9 +12,8 @@ WHERE login_time > '18:00' AND success = 0;
 #
 
 
-#By executing this SQL query, I aimed to retrieve all available information regarding the login attempts during the specified period. 
-#The query consisted of a WHERE clause that utilized the greater than or equal to (>=) 
-#and less than or equal to (<=) operators to encompass the dates May 8 and May 9, 2022.
+# Through the execution of this SQL query, my goal was to obtain all relevant information regarding the login attempts within the designated timeframe.
+# The query incorporated a WHERE clause utilizing the greater than or equal to (>=) and less than or equal to (<=) operators to cover the dates May 8 and May 9, 2022.
 
 SELECT *
 FROM log_in_attempts
@@ -23,8 +22,8 @@ WHERE login_date >= '2022-05-08' AND login_date <= '2022-05-09';
 #
 #
 
-# By executing this query, the login attempts that occurred outside of Mexico are identified. 
-# The results will provide insights into any suspicious activity originating from locations other than Mexico.
+# Through the execution of this query, it identifies the login attempts that took place outside of Mexico. The outcomes will offer insights into any suspicious 
+# activity originating from locations other than Mexico.
 
 SELECT *
 FROM log_in_attempts
@@ -32,7 +31,7 @@ WHERE country NOT LIKE 'MEX%';
 
 #
 #
-# This query allows for precise filtering, ensuring that only employees in the Marketing department and located in offices within the East building are included in the result set.  
+# This query enables precise filtering, guaranteeing that the result set includes only employees in the Marketing department situated in offices within the East building. 
 SELECT *
 FROM employees
 WHERE department = 'Marketing' AND office LIKE 'East-%';
